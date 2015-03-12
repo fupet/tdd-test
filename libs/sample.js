2b2bf85ac5a1022d3ad34db664c9ca262afc9bce
Peter Furst <peter.furst@doclerholding.com>
2015-03-12 17:24:51 +0100

    3. if first line is "#useFirstLineAsLabels"

--- libs/sample.js
+++ libs/sample.js
@@ -1,26 +1,23 @@
 var Sample = function() {};
 
+Sample.LABEL_IDENTIFIER = '#useFirstLineAsLabels\n';
+
 /**
  * Splits the input string into an array.
  * @param {string} input   The input to be split.
  * @returns {*}
  */
 Sample.prototype.stringToArray = function(input) {
-	if (typeof input === 'string') {
-		if (input.indexOf('\n') !== -1) {
-			input = input.split('\n');
-			for (var i in input) {
-				if (input.hasOwnProperty(i)) {
-					input[i] = this._splitStringByComa(input[i]);
-				}
-			}
-			return input;
-		}
-		return this._splitStringByComa(input);
-	}
-	else {
+	if (typeof input !== 'string') {
 		throw new TypeError('Only string type accepted!');
 	}
+	if (input.indexOf(Sample.LABEL_IDENTIFIER) === 0) {
+		return this._splitWithLabel(input);
+	}
+	else if (input.indexOf('\n') !== -1) {
+		return this._splitWithNewLine(input);
+	}
+	return this._splitStringByComa(input);
 };
 
 /**
@@ -31,6 +28,45 @@ Sample.prototype.stringToArray = function(input) {
  */
 Sample.prototype._splitStringByComa = function(input) {
 	return input.split(',');
+};
+
+/**
+ * If the string starts with label then use it.
+ * @param {string} input
+ * @returns {{labels: Array, data: Array}}
+ * @private
+ */
+Sample.prototype._splitWithLabel = function(input) {
+	input = input.substr(Sample.LABEL_IDENTIFIER.length).split('\n');
+	var out = {labels : [], data: []}, line, i;
+	for (i in input) {
+		if (input.hasOwnProperty(i)) {
+			line = this._splitStringByComa(input[i]);
+			if (parseInt(i, 10) === 0) {
+				out.labels = line;
+			}
+			else {
+				out.data.push(line);
+			}
+		}
+	}
+	return out;
+};
+
+/**
+ * The input string will be splitted into multi dimensional array by lines and comas.
+ * @param {string} input
+ * @returns {Array|*}
+ * @private
+ */
+Sample.prototype._splitWithNewLine = function(input) {
+	input = input.split('\n');
+	for (var i in input) {
+		if (input.hasOwnProperty(i)) {
+			input[i] = this._splitStringByComa(input[i]);
+		}
+	}
+	return input;
 };
 
 module.exports = Sample;
--- tests/sample.test.js
+++ tests/sample.test.js
@@ -3,22 +3,29 @@ var assert = require('chai').assert,
 
 suite('Sample test', function() {
 	var singleDataProvider = [
-		['a,b,c', ['a', 'b', 'c']],
-		['100,982,444,990,1', ['100','982','444','990','1']],
-		['Mark,Anthony,marka@lib.de', ['Mark','Anthony','marka@lib.de']]
-	],
-	multiDataProvider = [
-		['211,22,35\n10,20,33', [['211','22','35'], ['10','20','33']]],
-		['luxembourg,kennedy,44\nbudapest,expo\nter,5-7 gyors,fo utca,9', [['luxembourg','kennedy','44'],['budapest','expo'],['ter','5-7 gyors','fo utca','9']]]
-	],
-	testWithData = function(data) {
-		test('test data with provider', function() {
-			assert.deepEqual(this.sample.stringToArray(data[0]), data[1]);
-		});
-	};
+			['a,b,c', ['a', 'b', 'c']],
+			['100,982,444,990,1', ['100', '982', '444', '990', '1']],
+			['Mark,Anthony,marka@lib.de', ['Mark', 'Anthony', 'marka@lib.de']]
+		],
+		multiDataProvider = [
+			['211,22,35\n10,20,33', [['211', '22', '35'], ['10', '20', '33']]],
+			['luxembourg,kennedy,44\nbudapest,expo\nter,5-7 gyors,fo utca,9',
+				[['luxembourg', 'kennedy', '44'], ['budapest', 'expo'], ['ter', '5-7 gyors', 'fo utca', '9']]]
+		],
+		testWithData = function(data) {
+			test('test data with provider', function() {
+				assert.deepEqual(this.sample.stringToArray(data[0]), data[1]);
+			});
+		};
 	setup(function() {
 		this.sample = new Sample();
 
+	});
+
+	test('test with not string', function() {
+		assert.throws(function() {
+			this.sample.stringToArray(1);
+		}.bind(this), TypeError);
 	});
 
 	suite('one-line string input to array', function() {
@@ -27,5 +34,15 @@ suite('Sample test', function() {
 
 	suite('multi-line string input to array', function() {
 		multiDataProvider.forEach(testWithData, this);
+	});
+
+	test('use labels', function() {
+		assert.deepEqual(this.sample.stringToArray(
+				'#useFirstLineAsLabels\nName,Email,Phone\nMark,marc@be.com,998\nNoemi,noemi@ac.co.uk,888'),
+			{
+				labels : ['Name', 'Email', 'Phone'],
+				data   : [['Mark', 'marc@be.com', '998'], ['Noemi', 'noemi@ac.co.uk', '888']]
+			}
+		);
 	});
 });
