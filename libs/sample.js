73ad951c10094bba94829a9cdc685be9d1f56fae
Peter Furst <peter.furst@doclerholding.com>
2015-03-12 16:51:42 +0100

    2. multi-line string input

--- libs/sample.js
+++ libs/sample.js
@@ -1,17 +1,36 @@
 var Sample = function() {};
 
 /**
- * Splits the string into array.
- * @param input
- * @returns {string[]}
+ * Splits the input string into an array.
+ * @param {string} input   The input to be split.
+ * @returns {*}
  */
 Sample.prototype.stringToArray = function(input) {
 	if (typeof input === 'string') {
-		return input.split(',');
+		if (input.indexOf('\n') !== -1) {
+			input = input.split('\n');
+			for (var i in input) {
+				if (input.hasOwnProperty(i)) {
+					input[i] = this._splitStringByComa(input[i]);
+				}
+			}
+			return input;
+		}
+		return this._splitStringByComa(input);
 	}
 	else {
 		throw new TypeError('Only string type accepted!');
 	}
+};
+
+/**
+ * Splits the input string by comas and returns an array.
+ * @param {string} input   The input string to be splitted.
+ * @returns {Array|*}
+ * @private
+ */
+Sample.prototype._splitStringByComa = function(input) {
+	return input.split(',');
 };
 
 module.exports = Sample;
--- tests/sample.test.js
+++ tests/sample.test.js
@@ -2,21 +2,30 @@ var assert = require('chai').assert,
 	Sample = require('../libs/sample');
 
 suite('Sample test', function() {
-	var dataProvider = [
+	var singleDataProvider = [
 		['a,b,c', ['a', 'b', 'c']],
 		['100,982,444,990,1', ['100','982','444','990','1']],
 		['Mark,Anthony,marka@lib.de', ['Mark','Anthony','marka@lib.de']]
-	];
+	],
+	multiDataProvider = [
+		['211,22,35\n10,20,33', [['211','22','35'], ['10','20','33']]],
+		['luxembourg,kennedy,44\nbudapest,expo\nter,5-7 gyors,fo utca,9', [['luxembourg','kennedy','44'],['budapest','expo'],['ter','5-7 gyors','fo utca','9']]]
+	],
+	testWithData = function(data) {
+		test('test data with provider', function() {
+			assert.deepEqual(this.sample.stringToArray(data[0]), data[1]);
+		});
+	};
 	setup(function() {
 		this.sample = new Sample();
 
 	});
 
 	suite('one-line string input to array', function() {
-		dataProvider.forEach(function(data) {
-			test('test data with provider', function() {
-				assert.deepEqual(this.sample.stringToArray(data[0]), data[1]);
-			});
-		});
+		singleDataProvider.forEach(testWithData, this);
+	});
+
+	suite('multi-line string input to array', function() {
+		multiDataProvider.forEach(testWithData, this);
 	});
 });
