df51a3f2094896cde5d6b7f81780462601e462b7
Peter Furst <peter.furst@doclerholding.com>
2015-03-12 16:14:51 +0100

    1. one-line string input

--- libs/sample.js
+++ libs/sample.js
@@ -1,10 +1,17 @@
 var Sample = function() {};
 
 /**
- * Returns with number 1
+ * Splits the string into array.
+ * @param input
+ * @returns {string[]}
  */
-Sample.prototype.returnWithNumberOne = function() {
-	return 1;
+Sample.prototype.stringToArray = function(input) {
+	if (typeof input === 'string') {
+		return input.split(',');
+	}
+	else {
+		throw new TypeError('Only string type accepted!');
+	}
 };
 
 module.exports = Sample;
--- tests/sample.test.js
+++ tests/sample.test.js
@@ -2,11 +2,21 @@ var assert = require('chai').assert,
 	Sample = require('../libs/sample');
 
 suite('Sample test', function() {
+	var dataProvider = [
+		['a,b,c', ['a', 'b', 'c']],
+		['100,982,444,990,1', ['100','982','444','990','1']],
+		['Mark,Anthony,marka@lib.de', ['Mark','Anthony','marka@lib.de']]
+	];
 	setup(function() {
 		this.sample = new Sample();
+
 	});
 
-	test('Sample should return with 1', function() {
-		assert.equal(this.sample.returnWithNumberOne(), 1, 'Boo.');
+	suite('one-line string input to array', function() {
+		dataProvider.forEach(function(data) {
+			test('test data with provider', function() {
+				assert.deepEqual(this.sample.stringToArray(data[0]), data[1]);
+			});
+		});
 	});
 });
